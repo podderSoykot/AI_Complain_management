a969623ebc190c94202d6537fa95c6d97eff1d9b
Soykot Podder <diptopodder95@gmail.com>
2026-04-19 10:35:56 +0600

    add CROS origin

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://ai-complain-management.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
